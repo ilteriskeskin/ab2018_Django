c37964903644043265705e9018142320bf9320a2
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-01-29 10:38:19 +0300

    Post method app

--- election/views.py
+++ election/views.py
@@ -2,10 +2,14 @@ from django.http import *
 from django.shortcuts import render
 
 def home(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
         ad = request.GET['ad']
         email = request.GET['email']
-    return render(request, 'index.html', {"name": ad, "email": email})
+        data = "%s, %s" % (ad, email)
+        return render(request, 'index.html', {"data": data})
+
+    else:
+        return render(request, "index.html")
 
 def deneme(request):
     return render(request, 'deneme.html', locals())
